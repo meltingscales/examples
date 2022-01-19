421b2bc7523177724e19e75d8f025c7671d0b99e
Henry Post <HenryFBP@gmail.com>
2022-01-19 10:51:38 -0500

    simplify main loop

--- powershell/download-yt-vids/download_youtube_videos.ps1
+++ powershell/download-yt-vids/download_youtube_videos.ps1
@@ -1,8 +1,22 @@
+function downloadMP3 {
+    param (
+        $url
+    )
+    python -m youtube_dl -x -k --audio-format mp3 $url   
+}
+
+function downloadVideo {
+    param (
+        $url
+    )
+    python -m youtube_dl $url
+}
+
 Push-Location "./media/"
 
 get-content "../links.txt" | ForEach-Object {
 
-    if ("$_".Substring(0,1).Contains("#")) {
+    if ("$_".Substring(0, 1).Contains("#")) {
         Write-Host "Skipping comment $_";
     }
     elseif ([string]::IsNullOrWhiteSpace("$_")) {
@@ -10,11 +24,16 @@ get-content "../links.txt" | ForEach-Object {
     }
     else {
 
-        url = "$_".Split('#')[0]
+        parts = "$_".Split('#')
+        url = parts[0]
 
-        write-host "downloading" $_
+        if (parts.Length -gt 1) {
+            write-host "Ignoring comment: " parts[1]
+        }
 
-        python -m youtube_dl $_
+        write-host "downloading" $_ " as video"
+
+        downloadVideo $_
     }
 }
 
