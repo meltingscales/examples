43219152143a5df69b46ed51d6a7b053652b4637
Henry Post <HenryFBP@gmail.com>
2022-05-23 14:42:04 -0400

    start working on making site elts

---
+++ javascript/scraping/credential.net/format-urls-and-titles.js
@@ -0,0 +1,55 @@
+//output from scraping script, just copy pasted.
+
+var theURLS = `https://www.credential.net/8d0f994b-dc09-4128-8f78-56df41fbc72a
+https://www.credential.net/082cdb72-b077-4069-b54c-fa86c6cb6379
+https://www.credential.net/d202cc5d-486f-42e4-a240-ba92f3ec67b2
+https://www.credential.net/dc9738af-157a-4f76-95af-7afae6245c1d
+https://www.credential.net/57545d23-8818-4156-830a-2e6fe177c518
+https://www.credential.net/db7bbff0-434b-40e8-a34b-42dc91beb5b6
+https://www.credential.net/2b6580bf-37e3-4806-baae-895354d982a4
+https://www.credential.net/3a799e3e-b6d3-45dd-8760-a7dd91d8dac7
+https://www.credential.net/626624e9-15d6-42b2-9338-f7493e4ab6bf
+https://www.credential.net/22970c17-113c-4202-9ba2-69596b1a2036
+https://www.credential.net/e62a0eab-4375-4aca-b91d-da37eccb1407
+https://www.credential.net/9019a5ac-ff72-40f0-9398-8bf7adabbbd2
+https://www.credential.net/b13a2b5e-1858-42c8-9456-af3f37241276
+https://www.credential.net/4e0b308c-c59b-43fc-87bd-a5fae345e659
+https://www.credential.net/e1e46801-9e00-443e-9187-196c6e272a28
+https://www.credential.net/0bf0377f-181d-4ee2-a91f-aabf0748005e
+https://www.credential.net/7efffb62-aacf-4a05-918b-0c8b0eab9368
+https://www.credential.net/ccc7f313-161f-4f45-a73c-9bfd30c30cb9
+https://www.credential.net/b8b40662-3a3c-45ee-a618-286dd2d7bbff
+https://www.credential.net/d6a73e00-d3c3-4a02-bc02-9ce4955fd41c
+https://www.credential.net/68996786-1989-4b7c-b0d4-b0299243a82a
+https://www.credential.net/5e477838-3c67-4d94-a7cb-10294830f352
+https://www.credential.net/76acfa0d-f616-433d-adb6-8e27bb51570c
+https://www.credential.net/87f6d278-644a-4818-bef7-e8247c77b1e7
+https://www.credential.net/54824fd5-5c58-41c7-8ff9-4bc9c12d19c5
+https://www.credential.net/64bf25db-1ace-4792-822d-7b45f7c41350`
+
+var theTitles = `Microsoft Azure Fundamentals: Virtual Machines & Security Groups
+Microsoft Azure Fundamentals: Security & Compliance
+Microsoft Azure Fundamentals: Software Development
+Microsoft Azure Fundamentals: Monitoring & Cost Management
+Microsoft Azure Fundamentals: Resource Governance
+Microsoft Azure Fundamentals: Azure AD & Windows Virtual Desktop
+Microsoft Azure Fundamentals: App Services
+Microsoft Azure Fundamentals: Databases & Data Analytics
+Microsoft Azure Fundamentals: Azure Storage
+Microsoft Azure Fundamentals: Networking
+Microsoft Azure Fundamentals: Managing Azure Services
+Microsoft Azure Fundamentals: Subscriptions & ARM Templates
+Microsoft Azure Fundamentals: Cloud Computing
+Cloud Computing Fundamentals: Risk Management
+Cloud Computing Fundamentals: Security
+Cloud Computing Fundamentals: Presence & Privacy
+AWS Cloud Practitioner 2020: Cloud Economics
+AWS Cloud Practitioner 2020: Cloud & the Value Proposition
+AWS Associate Solution Architect: AWS Primer
+Cloud Computing Fundamentals: Cloud Access Control
+Cloud Computing Fundamentals: Migrating to the Cloud
+Cloud Computing Fundamentals: Cloud Protocols & Standards
+Cloud Computing Fundamentals: Storing & Managing Cloud Data
+Cloud Computing Fundamentals: Cloud Virtualization & Data Centers
+Cloud Computing Fundamentals: Introduction
+Cloud Computing Fundamentals: Considerations`
