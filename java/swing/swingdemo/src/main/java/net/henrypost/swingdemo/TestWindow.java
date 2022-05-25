2bf6d601ffe459d2cb170eaeacf189eea5de7f4e
Henry Post <HenryFBP@gmail.com>
2022-05-25 14:59:01 -0400

    randomize window pos

--- java/swing/swingdemo/src/main/java/net/henrypost/swingdemo/TestWindow.java
+++ java/swing/swingdemo/src/main/java/net/henrypost/swingdemo/TestWindow.java
@@ -3,16 +3,22 @@ package net.henrypost.swingdemo;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /*
  * You'll want to install https://www.eclipse.org/windowbuilder/
  */
 
 public class TestWindow {
+
+	public static final int gimmeRandXY() {
+		return new Random().nextInt(300);
+	}
 
 	private JFrame frame;
 
@@ -46,17 +52,22 @@ public class TestWindow {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JButton button = new JButton();
 		button.setLabel("click me!");
-		
+
 		button.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent arg0) {
-				JDialog x = new JDialog();
-				x.setTitle("Wow! You clicked me!");
-				x.setBounds(0, 0, 200, 200);
-				x.show();
+				JDialog popup = new JDialog();
+				popup.setTitle("Wow! You clicked me!");
+				popup.setBounds(gimmeRandXY(), gimmeRandXY(), gimmeRandXY(), gimmeRandXY());
+
+				JLabel label = new JLabel();
+				label.setText("nice clicking :)");
+				popup.add(label);
+
+				popup.show();
 			}
 		});
 		frame.add(button);
