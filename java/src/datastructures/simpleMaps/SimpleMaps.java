9be611957da112cc125cdf580e31d098f2ffd77c
Henry Post <HenryFBP@gmail.com>
2022-05-27 09:43:50 -0400

    add simple map example

---
+++ java/src/datastructures/simpleMaps/SimpleMaps.java
@@ -0,0 +1,31 @@
+package datastructures.simpleMaps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//  to run: 
+//  javac SimpleMaps.java; java SimpleMaps
+
+public class SimpleMaps {
+
+	public static void printMap(Map<String, String> stringMap) {
+		stringMap.forEach((key, value) -> {
+			System.out.println("Key: " + key + ", value: " + value);
+		});
+	}
+
+	public static void main(String[] args) {
+		System.out.println(SimpleMaps.class.getSimpleName());
+
+		Map<String, String> someMap = new HashMap();
+
+		someMap.put("name", "Henry");
+		someMap.put("age", "24");
+		someMap.put("favorite food", "steak");
+
+		System.out.println("we have " + someMap.keySet().size() + " keys");
+
+		printMap(someMap);
+
+	}
+}
