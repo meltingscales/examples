3381da1c396ac04a6bf0a3ec4e9ec55d4b625b31
Henry Post <HenryFBP@gmail.com>
2022-05-26 20:08:35 -0400

    add simple c strtok

---
+++ c/strings/simple.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+
+    char *inp = "123.123.123";
+
+    char *tokVal = strtok(inp, ".");
+
+    while(tokVal != NULL){
+        
+    }
+}
