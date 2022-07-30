70049d6203336a95ec0cc254112b33896400078f
Henry Post <HenryFBP@gmail.com>
2022-07-30 16:09:41 -0400

    add counter

--- react/my_first_react_app/src/App.tsx
+++ react/my_first_react_app/src/App.tsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+
+const Counter = () => {
+
+    var initialNumber = 0
+
+    const [counter, setCounter] = useState(initialNumber)
+    return (
+        <>
+
+            <button onClick={() => {
+                setCounter(counter - 1)
+            }}>
+                -
+            </button>
+            <h1>{counter}</h1>
+            <button onClick={() => {
+                setCounter(counter + 1)
+            }}>
+                +
+            </button>
+
+        </>
+    )
+}
 
 const Person = (props: any) => {
 
@@ -18,7 +42,7 @@ const Person = (props: any) => {
         <>
             {eltName}
             {eltLastName}
-            <h2>Age: 24</h2>
+            <h2>Age: {props.age}</h2>
         </>
     )
 }
@@ -27,6 +51,7 @@ const App = () => {
 
     const name = 'Henry'
     const lname = 'Post'
+    const age = 24
 
     const randFloat = Math.random()
 
@@ -35,14 +60,21 @@ const App = () => {
     const personProps = {
         name: name,
         lastName: lname,
-        nameHidden: isNameHidden
+        nameHidden: isNameHidden,
+        age: age
     }
 
     return (
         <div className="App">
+            <Counter></Counter>
             <p>randFloat={("" + randFloat).slice(0, 4)}</p>
             <h1>hello react :3c</h1>
             <Person {...personProps}/>
+            <Person
+                name={'Jane'}
+                lastName={'Doe'}
+                age={29 + 1}
+            />
         </div>
     );
 }
