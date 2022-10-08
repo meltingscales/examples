8a345b93264147857eebc2092e227a5f8320a6d9
Henry Post <HenryFBP@gmail.com>
2022-10-08 12:22:41 -0400

    start collision detection

---
+++ python3/games/collision-detection-al-sweigart/collisionDetection.py
@@ -0,0 +1,13 @@
+# from Invent Your Own Computer Games with Python, 4th Edition Paperback
+#   Al Sweigart
+
+
+import pygame
+import sys
+import random
+
+pygame.init()
+
+mainclock = pygame.time.Clock()
+
+# TODO - finish the rest of this
