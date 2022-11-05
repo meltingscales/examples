b0a8033fdbf7106359d9b3bb9358587a7a97ebf6
Henry Post <HenryFBP@gmail.com>
2022-11-05 17:47:10 -0400

    add 2nd arg

--- bash/yt-dlp/download-and-merge-into-the-cloud.sh
+++ bash/yt-dlp/download-and-merge-into-the-cloud.sh
@@ -5,10 +5,10 @@ URLS2=https://www.youtube.com/watch?v=rFbQd7VIILc
 function ensure_exists {
     if ! command -v $1 &> /dev/null
     then
-        echo "The command $1 could not be found"
+        echo "The command $1 could not be found. Try $2"
         exit
     fi
 }
 
-ensure_exists "ffmpeg"
-ensure_exists "yt-dlp"
+ensure_exists "ffmpeg" "apt install ffmpeg"
+ensure_exists "yt-dlp" "pip install yt-dlp"
