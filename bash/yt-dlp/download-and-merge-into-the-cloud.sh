24e0939f3956b629582bed419c4b07afb02fc6c9
Henry Post <HenryFBP@gmail.com>
2022-11-05 22:40:39 +0000

    get 2 files to download

--- bash/yt-dlp/download-and-merge-into-the-cloud.sh
+++ bash/yt-dlp/download-and-merge-into-the-cloud.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 URLS1=https://vimeo.com/390580961
+FILES1=intoTheCloudSeasonOne.mp4
+FORMATS1="intoTheCloudSeasonOne.%(ext)s"
+
 URLS2=https://www.youtube.com/watch?v=rFbQd7VIILc
+FILES2=intoTheCloudSeasonTwo.mp4
+FORMATS2="intoTheCloudSeasonTwo.%(ext)s"
 
 function ensure_exists {
     if ! command -v $1 &> /dev/null
@@ -11,4 +16,16 @@ function ensure_exists {
 }
 
 ensure_exists "ffmpeg" "apt install ffmpeg"
-ensure_exists "yt-dlp" "pip install yt-dlp"
+ensure_exists "yt-dlp" "snap install yt-dlp"
+
+if [ ! -f $FILES1 ]; then
+    yt-dlp $URLS1 -o $FORMATS1 -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
+else 
+    echo "$FILES1 already exists"
+fi
+
+if [ ! -f $FILES2 ]; then
+    yt-dlp $URLS2 -o $FORMATS2 -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
+else 
+    echo "$FILES2 already exists"
+fi
