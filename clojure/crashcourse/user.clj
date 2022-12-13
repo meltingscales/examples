6020b133b0bf9568a505f6497fbebe271c9c6da9
Henry Post <HenryFBP@gmail.com>
2022-12-13 12:13:59 -0500

    start

---
+++ clojure/crashcourse/user.clj
@@ -0,0 +1,13 @@
+;; run clj and copy paste
+;; or use vim
+;; or use vscode, calva, and use CTRL-SHIFT-P to run "Calva Jack-in" - just choose "deps.edn"
+;; make sure if u use vscode, the only open folder is this one, "crashcourse"
+(println "Hello world")
+
+(+ 1 1)
+
+(+ 3 3 
+    (* 2 2))
+
+(+ 1 1)
+
