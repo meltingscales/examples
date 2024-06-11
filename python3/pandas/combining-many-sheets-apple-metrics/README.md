afd9e1bb8326855306964e5e81cdba042b77978a
Henry Post <HenryFBP@gmail.com>
2024-06-11 11:36:55 -0500

    generate data

---
+++ python3/pandas/combining-many-sheets-apple-metrics/README.md
@@ -0,0 +1,9 @@
+# apple metrics
+
+This is a sample program meant to show you how to combine many excel sheets into 1.
+
+It's meant to track apple farmer yields.
+
+1. `python generate-data.py`. This generates 20 weekly metrics.
+2. `python combine-data.py`. This combines those weekly metrics into 1 sheet.
+3. View `combined-apple-metrics.xlsx`.
---
+++ python3/pandas/combining-many-sheets-apple-metrics/generate-data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import os
+import shutil
+
+# Remove the data folder if it exists
+if(os.path.exists('data')):
+    shutil.rmtree('data')
+
+# Create the data folder
+os.makedirs('data', exist_ok=True)
+
+# Generate 20 excel files with random data
+for i in range(0, 20):
+
+    df = pd.DataFrame({
+        # Create a list of 4 farmer names
+        "Farmer name": ['John', 'Paul', 'George', 'Ringo'],
+
+        # Create a list of 4 random apple counts between 0 and 1000
+        'Apple count': np.random.randint(0, 1000, 4),
+    })
+
+    # Save the data to an excel file
+    filepath='data/apples_week_{}.xlsx'.format(i+1)
+    print("Saving to: ", filepath)
+    df.to_excel(filepath, index=False)
