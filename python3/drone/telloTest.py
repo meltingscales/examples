a21b89de6aa4ae752333bc1953c4994f9722d16c
Henry Post <HenryFBP@gmail.com>
2025-12-23 22:06:55 -0600

    asfdasdfasfasdf

--- python3/drone/telloTest.py
+++ python3/drone/telloTest.py
@@ -3,11 +3,18 @@
 # pip install -r requirements.txt
 
 from djitellopy import Tello
+import time
 
 tello = Tello()
 
 tello.connect()
+
+# Check battery level
+print(f"Battery: {tello.get_battery()}%")
+
 tello.takeoff()
+
+time.sleep(8)  # Give the drone more time to stabilize after takeoff
 
 tello.move_left(100)
 tello.rotate_counter_clockwise(90)
